05a7c3cbffc3991642da3af31278ee8ef340a6af
rahulpandika <rahulpandika@gmail.com>
2020-05-15 01:37:19 +0700

    first commit

---
+++ app/src/main/java/com/example/modul6/Konfigurasi.java
@@ -0,0 +1,26 @@
+package com.example.modul6;
+
+public class Konfigurasi {
+
+    public static final String URL_ADD="http://192.168.1.107/Praktikum6/insert.php";
+    public static final String URL_GET_ALL = "http://192.168.1.107/Praktikum6/read.php";;
+    public static final String URL_GET_MHS = "http://192.168.1.107/Praktikum6/select.php";;
+    public static final String URL_UPDATE_MHS = "http://192.168.1.107/Praktikum6/update.php";;
+    public static final String URL_DELETE_MHS = "http://192.168.1.107/Praktikum6/delete.php?id=";
+
+
+    public static final String KEY_MHS_ID = "id";
+    public static final String KEY_MHS_NAMA = "nama";
+    public static final String KEY_MHS_JURUSAN = "jurusan";
+    public static final String KEY_MHS_EMAIL = "email";
+
+
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_JURUSAN = "jurusan";
+    public static final String TAG_EMAIL = "email";
+
+
+    public static final String MHS_ID = "mhs_id";
+}
